80d31d210cdf3e081dd29f3ec94aa29122cf315e
mrascii <24985729+mrascii@users.noreply.github.com>
2019-12-31 01:03:39 +0100

    Moved search indexes into a common static folder
    Due to regression after hugo 0.56+, root folder for multilang sites without unique baseURL
    does not merge static files from different staticDir2 etc folders any more.

--- tools/build_search_index.sh
+++ tools/build_search_index.sh
@@ -23,16 +23,15 @@ pushd "$SCRIPT_DIR/.."
   hugo --environment searchindex -d "$SEARCH_INDEX_ROOT"
 popd
 
-# TODO: Add hash to file names
 for lang in "${LANGUAGES[@]}"
 do
   input="$SCRIPT_DIR/../$SEARCH_INDEX_ROOT/$lang/search.json"
   output="${input}.js"
   node "$SCRIPT_DIR/build_search_index.js" "$lang" "$input" "$output"
   MD5_SUM=$(md5_of_file "$output")
-  hashed_output="$SCRIPT_DIR/../static/$lang/search-$MD5_SUM.json.js"
+  hashed_output="$SCRIPT_DIR/../static/search-$lang-$MD5_SUM.json.js"
   # Remove previous versions (if any)
-  rm -f "$SCRIPT_DIR/../static/$lang/search-*.json.js"* || true
+  rm -f "$SCRIPT_DIR/../static/search-$lang-"*".json.js"* || true
   mv "$output" "$hashed_output"
   # Pre-compress search index for the web server. Compressed version takes less time to download by clients.
   gzip -f -k -9 "$hashed_output"
